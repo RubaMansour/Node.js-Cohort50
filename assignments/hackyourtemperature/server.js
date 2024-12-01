e3a4f11e50bc48452fef377a5b28809aaad1c91f
ruba <rubamansour73@gmail.com>
2024-12-01 15:45:27 +0100

    modify requirements

--- assignments/hackyourtemperature/server.js
+++ assignments/hackyourtemperature/server.js
@@ -1,14 +1,18 @@
-
 import express from 'express';
-
 const app = express();
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('hello from backend to frontend!');
 });
-app.use(express.json());
+
 app.post('/weather', (req, res) => {
   const { cityName } = req.body;
-  res.send(`Received city: ${cityName}`);
-});
+  if (!cityName) {
+    return res.status(400).json({ error: 'There is no city'});
+  }
+   res.status(200).json({ message: 'received City', cityName });
+})
 
 app.listen(3000);
